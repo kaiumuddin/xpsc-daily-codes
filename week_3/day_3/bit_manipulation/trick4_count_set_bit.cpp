fefc1af7c51c7619f22c0772ce46a1ca27f59037
kaiumuddin <kaium.uddin2909@gmail.com>
2023-10-18 23:42:07 +0600

    week 3

---
+++ week_3/day_3/bit_manipulation/trick4_count_set_bit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    while (n > 0)
+    {
+        cnt += n & 1;
+        n >>= 1;
+    }
+
+    cout << cnt << endl;
+}
